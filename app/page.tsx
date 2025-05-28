2508d0998a22b171820e3c486329e163b9009698
siamliam12 <siamahmed222945@gmail.com>
2025-05-28 15:51:35 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="text-2xl">Welcome</h1>
+      <Button>Let us start</Button>
+    </div>
+  )
+}
+
+export default Page
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const signIn = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default signIn
